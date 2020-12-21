84907ca735418691f4492d1404e46535baae49cc
cibezimako <74436602+cibezimako@users.noreply.github.com>
2020-12-20 20:13:34 -0500

    hopefully, this is last edit

--- videogame_generator_GUI_beta.py
+++ videogame_generator_GUI_beta.py
@@ -14,12 +14,12 @@ class game_Generator:
     def __init__(self):
         self.root = tk.Tk()#refers to the window
         self.root.title("Videogame Suggestion Generator")
-        self.root.geometry("800x400")
+        self.root.geometry("1000x400")
         self.v = tk.IntVar()
-        #self.consoles =[]#this needs to be populated first
-        #self.titles = []#this needs to be populated first
+        #self.consoles =[""]#this needs to be populated first
+        #self.titles = [""]#this needs to be populated first
         self.consoles =['XB', 'PS']#for testing purposes
-        self.titles=  ['Star Wars: Battlefront', 'Madden NFL 06', 'STORM: Frontline Nation', 'Men in Black II: Alien Escape']#for testing purposes
+        self.titles=  ['']#for testing purposes
         self.fname = 'Video_Games_Sales_as_at_22_Dec_2016.csv'
         self.df = videogamessales.make_games_clean(self.fname)
         self.gameList=['']
@@ -79,7 +79,11 @@ class game_Generator:
             self.csv_reader = csv.DictReader(gameFile, delimiter=',')
             for lines in self.csv_reader:
                 self.gameList.append(lines['Name'])
+               
         return self.gameList
+    
+       
+        
         
     def user_age(self):
         self.age_var = tk.IntVar()
@@ -103,15 +107,17 @@ class game_Generator:
         self.lbl_game = tk.Label(self.root, text = "Owned games: ").grid(row=3, column=0,padx=5, pady=5)
         
         
-        self.combo_games = ttk.Combobox(self.root, values=self.gameList).grid(row=3,column=1, padx=1, pady=1)
+        self.combo_games = ttk.Combobox(self.root, textvariable= self.gameType_var , values=self.gameList).grid(row=3,column=1, padx=1, pady=1)
         
         
         #self.entry_gameType = tk.Entry(self.root, textvariable= self.gameType_var, bd =5)
     def gen_lbl(self):
-        self.recommend = tk.StringVar()
-        self.recommend.set("Recommendations will be displayed here")
+        #self.recommend = tk.StringVar()
+        #self.recommend.set("Recommendations will be displayed here")
         
-        label_gen = Label(self.root, textvariable= self.recommend).grid(row=10, column= 1, padx=5, pady=5)
+        self.label_gen = Label(self.root, text="Recommendations will be displayed here")
+        self.label_gen.grid(row=10, column= 1, padx=5, pady=5)
+    
     
     def record_entries(self):
         self.name_input = self.name_var.get()#retieves the value in the name textbox
@@ -122,10 +128,21 @@ class game_Generator:
         #videogamessales.my_age = self.age_input
         
     def add_titles(self):
-        self.record_entries()#just pre-loads entries inputed by user
-        self.titles.append(self.game_input)
-        #self.recommend.set(self.titles)
+        #self.record_entries()#just pre-loads entries inputed by user
+        try:
+            self.record_entries()
+            #self.gameType_var.get()
+            #self.titles.append(self.game_input)
+            #self.titles.append(self.game_input)
+        except:
+            print("List cannot be empty!")
+        finally:
+            self.titles.append(self.game_input)
         
+        
+        #self.recommend.set(self.titles)
+    
+    
     
     def add_consoles(self):
         self.record_entries()#just pre-loads entries inputed by user
@@ -135,17 +152,23 @@ class game_Generator:
         self.consoles.append(self.game_console)
         #self.recommend.set(self.consoles)
         
-    def gen_lbl(self):
-        self.recommend = tk.StringVar()
-        self.recommend.set("Recommendations will be displayed here")
+    def show_titles_lbl(self):
+        self.record_entries()
+        #self.games = tk.StringVar()
+        #self.games.set("List will be displayed here")
         
-        label_gen = Label(self.root, textvariable= self.recommend).grid(row=10, column= 1, padx=5, pady=5)
+        self.lbl_games = Label(self.root, text="\n".join(self.titles)).grid(row=3, column= 4, padx=5, pady=5)
+        #self.lbl_games.config(text='')
+        #self.lbl_games.config(test=("\n".join(self.gameList)))
         
     
     def btn_add_titles(self):
         btn_gen = tk.Button(self.root, text = "Add Games", command = self.add_titles).grid(row=3, column=2, padx=5, pady=5)
-        
     
+    def btn_show_titles(self):
+        self.record_entries()
+        btn_gen = tk.Button(self.root, text = "Preview List", command = self.show_titles_lbl).grid(row=3, column=3, padx=5, pady=5)
+            
     def btn_add_consoles(self):
         btn_gen = tk.Button(self.root, text = "Add Console", command = self.add_consoles).grid(row=1, column=2, padx=5, pady=5)
         
@@ -160,8 +183,17 @@ class game_Generator:
         #         messagebox.showinfo("Age", "Age is {}".format(self.age_input)) #just to test code. not final edit
         #     pass
         # messagebox.showinfo("Name", "Name is {}".format(self.name_input)) #not final edit
-        self.recommend.set(videogamessales.suggest_games(df,self.titles, self.consoles, self.age_input,3))#for list or csv file with game recmmendatins
-        #"We recommend the following games for you {}".format(self.name_input) 
+        try:
+            self.label_gen.configure(text="{}, we recommend ".format(self.name_input)+
+                       str(videogamessales.suggest_games(self.df,self.titles, self.consoles, self.age_input,3))
+                       + " to play next")#for list or csv file with game recommendations
+            pass
+        except AttributeError:
+            messagebox.showinfo("Information Needed", "Please make sure all fields are filled")
+        finally:
+            pass
+        
+        
         
         
         
@@ -178,18 +210,29 @@ class game_Generator:
     
     
 def main():
+    #methods to function the buttons
     gen = game_Generator()
     gen.list_of_games()
     gen.user_name()
     gen.console()
     gen.user_age()
     gen.game_owned()
+    
+    #methods for labels
+    gen.show_titles_lbl()
     gen.gen_lbl()
-    gen.record_entries()
+    
+    
+    #gen.record_entries()
+    
+    #methods for buttons
     gen.btn_add_consoles()
     gen.btn_add_titles()
+    gen.btn_show_titles()
     gen.btn_generate()
-    gen.switch_settings()
+    #gen.switch_settings()
+    
+    
     gen.run_tk()
         
 if __name__ == "__main__":
