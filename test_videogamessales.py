12b92b6e6083f12cc7690a04c9f26cae7aa596ee
smobarry <smobes217@gmail.com>
2020-12-20 23:03:39 -0500

    Updated code base of unit tests

--- test_videogamessales.py
+++ test_videogamessales.py
@@ -1,15 +1,19 @@
-# Happy path
-# inputs and expected outputs are "normal"
-# Edge cases ("unhappy path")
-# inputs are unusual or special values
-# may trigger exceptions
+# Scott Mobarry
+# INST 326
+
+"""
+This is how you run these tests:
+python -m pylint videogamessales.py
+"""
 
 import pytest
 import pandas
 from videogamessales import make_games_clean, videogames_more_like_this, videogames_filtering, videogames_sampling, suggest_games
  
 def startup():
-
+    """
+    A common set of values for these tests.
+    """
     my_df = pandas.DataFrame({
         "Name": ["Game A", "Game B", "Game C", "Game D", "Game E", "Game F"],
         "Platform": ["XB", "Wii", "XB", "PS", "XB", "PS"],
@@ -25,29 +29,22 @@ def startup():
     my_platforms = ['XB', 'PS']
     my_age = 16
     num_suggestions = 2
-
-    if False:
-        the_suggestions = suggest_games(
-            my_df,
-            my_titles,
-            my_platforms,
-            my_age,
-            num_suggestions,
-            )
-        print(f'{len(the_suggestions)} suggestions = {the_suggestions}')
     return (my_df, my_titles, my_platforms, my_age, num_suggestions)
 
 
 def test_videogames_more_like_this():
+    """
+    Testing the happy path for the more like this.
+    """
     (my_df, my_titles, my_platforms, my_age, num_suggestions) = startup()
-    print('----- test_videogames_more_like_this +++++')
-    print(f'my_df = {my_df}')
-    found_genres: pandas.Series = videogames_more_like_this(my_df, my_titles)
-    print(f'found_genres ={found_genres}')
+    found_genres = videogames_more_like_this(my_df, my_titles)
     assert set(found_genres) == set(["Racing", "Action"])
 
 
 def test_videogames_filtering():
+    """
+    Testing the happy path.
+    """
     (my_df, my_titles, my_platforms, my_age, num_suggestions) = startup()
     found_genres = pandas.Series(["Racing", "Action"])
     df_can_suggest = videogames_filtering(my_df, found_genres, my_platforms, my_age, my_titles)
@@ -55,6 +52,9 @@ def test_videogames_filtering():
 
 
 def test_videogames_sampling():
+    """
+    Testing the happy path. Because the sampling was random the random state needed to be set to a constant.
+    """
     df_can_suggest = pandas.DataFrame({
         "Name": ["Game A", "Game B", "Game C", "Game D", "Game E", "Game F"],
         "Platform": ["XB", "Wii", "XB", "PS", "XB", "PS"],
@@ -66,6 +66,9 @@ def test_videogames_sampling():
     assert set(suggestions) == set(['Game B', 'Game C'])
 
 def test_suggest_games():
+    """
+    It tests calling the three sub methods all at once.
+    """
     (my_df, my_titles, my_platforms, my_age, num_suggestions) = startup()
     sugestions = suggest_games(
         my_df,
